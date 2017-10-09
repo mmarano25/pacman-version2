0b2beee8cf5c032de293ce4c2d445c4c838ae485
Marano <k1652879@nmscde004174.nms.kcl.ac.uk>
2017-10-09 16:16:30 +0100

    started work on new agent: CornerSeekingAgent

--- pacman/myAgents.py
+++ pacman/myAgents.py
@@ -4,17 +4,6 @@ import api
 import random
 import game
 import util
-"""
-class GoWestAgent(Agent):
-	def getAction(self, state):
-	    legal = api.legalActions(state)
-        if( Directions.WEST in legal ):
-            return api.makeMove(Directions.WEST,legal)
-        else:
-            if Directions.STOP in legal:
-                legal.remove(Directions.STOP)
-            return api.makeMove( random.choice(legal), legal)
-"""
 
 class GoWestAgent(Agent):
     def getAction(self, state):
@@ -28,12 +17,34 @@ class GoWestAgent(Agent):
 
 class CornerSeekingAgent(Agent):
     def __init__(self):
-        last = Directions.STOP
-        corners = []
+        self.last = Directions.STOP
+        self.cornersToSeek = list()
+        self.first_call = True
+        self.current_corner = ''
 
     def getAction(self,state):
+        if first_call:
+            for corner in api.corners(state):
+                self.cornersToSeek.append(corner)
+            self.first_call = False
+
         legal = api.legalActions(state)
-        corners = api.corners(state)
-        print corners
+        my_coord = api.whereAmI(state)
+        
+        
+
         return api.makeMove( random.choice(api.legalActions(state)), legal)
 
+
+# figure out how to get to a corner.
+#   see which direction corner is in: max(corner - my_coord)
+# for corners: 2 3
+#              0 1
+# 
+# 0: S E
+# 1: S W
+# 2: N E
+# 3: N W
+
+# note: only take action when last move is not legal.
+#     will that affect avoiding ghosts?
